fc0cb43035d92d8bd8d41f8e3fc2faf1182b3872
weicheng.lwc <weicheng.lwc@alibaba-inc.com>
2017-06-09 15:25:47 +0800

    增加package结构

---
+++ src/main/java/com/gua/open/jdbc/A.java
@@ -0,0 +1,9 @@
+package com.gua.open.jdbc;
+
+/**
+ * 类A.java的实现描述：TODO 类实现描述 
+ * @author weicheng.lwc 2017年6月9日 下午3:22:59
+ */
+public class A {
+
+}
---
+++ src/main/java/com/gua/open/jdbc/package-info.java
@@ -0,0 +1,6 @@
+
+/**
+ * 类package-info.java的实现描述：TODO 类实现描述 
+ * @author weicheng.lwc 2017年6月9日 下午3:21:27
+ */
+package com.gua.open.jdbc;
